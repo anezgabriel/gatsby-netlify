9e6d9c4cc72e2c91b3618c1b9697be4c63171bed
Gabriel Añez <artu242@gmail.com>
2020-05-05 10:55:29 -0400

    social

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@ module.exports = {
     author: `Solomedia Group`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    social: {
+      twitter: `smg`,
+    },
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
